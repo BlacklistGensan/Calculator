f3e2aa4260097d46ee05f7a43da18385c25306d3
BlacklistGensan <86349244+BlacklistGensan@users.noreply.github.com>
2021-07-09 17:42:40 +0800

    Create Calculator-Logic.py

---
+++ Calculator-Logic.py
@@ -0,0 +1,38 @@
+#Calculator Logic
+
+class Calculator:
+
+# Function to add in the entry of text display
+    def clickbut(self, number): 
+        global operator
+        operator = operator + str(number)
+        textin.set(operator)
+
+# Function to find the result of the operation
+    def evaluate(self) :
+        try:
+            global operator
+            input=str(eval(operator))
+            textin.set(input)
+            operator= ""
+        except: 
+            textin.set("ERROR")
+            operator = "" 
+            
+# Function that clears the whole entry of text display   
+    def clrALLbut(self):
+        global operator
+        textin.set("")
+        operator=""
+
+# Function to delete one by one from the last in the entry of text display
+    def backspacebut(self):
+        global operator
+        operator=operator[:-1]
+        textin.set(operator)
+    
+    
+c = Calculator()
+
+
+
